6e21fe4c2b913994380e49fd1da49d26fb95f3ea
bukvarik <59448145+bukvarik@users.noreply.github.com>
2020-07-16 22:38:30 +0300

    Add files via upload
    full project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var lightbox = new SimpleLightbox('.gallery a', {});
+
+
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
